618eae2498e892581a76cd0a64b3b3b1f0065e63
Stephan Wypler <s.wypler@online.de>
2013-10-03 14:19:40 +0200

    added missing Options.h

---
+++ src/Options.h
@@ -0,0 +1,23 @@
+/*
+ * Options.h
+ *
+ *  Created on: Oct 2, 2013
+ *      Author: stephan
+ */
+
+#ifndef OPTIONS_H_
+#define OPTIONS_H_
+
+struct Options{
+	std::string broker_hostname;
+	std::string broker_port;
+	std::string broker_ca;
+	std::string broker_tls_version;
+	bool broker_tls;
+	std::string ws_crt;
+	std::string ws_key;
+	std::string ws_dh;
+	bool ws_tls;
+};
+
+#endif /* OPTIONS_H_ */
