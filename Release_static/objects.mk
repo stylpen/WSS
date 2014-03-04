f3b5e74fe94ca10928d3134d9c5d1064cb5121b9
Stephan Wypler <s.wypler@online.de>
2014-03-04 15:31:20 +0100

    new Makefile and websocketpp submodule

--- Release_static/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /usr/local/lib/libwebsocketpp.a /usr/lib/libboost_thread.a /usr/lib/libboost_regex.a /usr/lib/libboost_system.a /usr/lib/libboost_program_options.a
-
-LIBS := -lpthread
-
